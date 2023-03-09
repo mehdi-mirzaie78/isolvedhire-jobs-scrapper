672a4d29542488f3a273f921e00e124ac324d980
mehdi <mehdimirzaie1378@gmail.com>
2023-03-09 12:40:14 +0330

    forms.py | Added extra fields for filtering

--- isolvehire_jobs_scrapper_django/jobs_board_scrapper_app/forms.py
+++ isolvehire_jobs_scrapper_django/jobs_board_scrapper_app/forms.py
@@ -2,6 +2,11 @@ from django import forms
 
 
 class JobForm(forms.Form):
-    title = forms.CharField(required=False, max_length=120, label="Title", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    title = forms.CharField(required=False, label="Title", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    location = forms.CharField(required=False, label="Location", widget=forms.TextInput(attrs={'class': 'form-control'}) )
+    payment_start = forms.FloatField(required=False, label="Payment Start", widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    payment_end = forms.FloatField(required=False, label="Payment End", widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    payment_type = forms.CharField(required=False, label="Payment Type", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    employment_type = forms.CharField(required=False, label="Employment Type", widget=forms.TextInput(attrs={'class': 'form-control'}))
     start_date = forms.DateField(required=False, label='Start Date', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
     end_date = forms.DateField(required=False, label='Start Date', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
