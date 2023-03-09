833ff0a56d305757b60d60f3f51582c499df05d3
mehdi <mehdimirzaie1378@gmail.com>
2023-03-09 14:26:42 +0330

    forms | Modified payment field

--- isolvehire_jobs_scrapper_django/jobs_board_scrapper_app/forms.py
+++ isolvehire_jobs_scrapper_django/jobs_board_scrapper_app/forms.py
@@ -4,8 +4,7 @@ from django import forms
 class JobForm(forms.Form):
     title = forms.CharField(required=False, label="Title", widget=forms.TextInput(attrs={'class': 'form-control'}))
     location = forms.CharField(required=False, label="Location", widget=forms.TextInput(attrs={'class': 'form-control'}) )
-    payment_start = forms.FloatField(required=False, label="Payment Start", widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    payment_end = forms.FloatField(required=False, label="Payment End", widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    payment = forms.CharField(required=False, label="Payment", widget=forms.NumberInput(attrs={'class': 'form-control'}))
     payment_type = forms.CharField(required=False, label="Payment Type", widget=forms.TextInput(attrs={'class': 'form-control'}))
     employment_type = forms.CharField(required=False, label="Employment Type", widget=forms.TextInput(attrs={'class': 'form-control'}))
     start_date = forms.DateField(required=False, label='Start Date', widget=forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}))
